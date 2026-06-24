4d5c4eed21ea1fd06e8cb29f6b809c59606f64e3
Riza Yakub <afrizal.090477@gmail.com>
2026-06-24 08:55:11 +0700

    feat: add navigation data

---
+++ src/constant/navigation-data.ts
@@ -0,0 +1,31 @@
+export interface NavigationItem {
+  label: string;
+  href: string;
+}
+
+export const navigationData: NavigationItem[] = [
+  {
+    label: "Home",
+    href: "#home",
+  },
+  {
+    label: "About",
+    href: "#about",
+  },
+  {
+    label: "Skill",
+    href: "#skill",
+  },
+  {
+    label: "Project",
+    href: "#project",
+  },
+  {
+    label: "FAQ",
+    href: "#faq",
+  },
+  {
+    label: "Contact",
+    href: "#contact",
+  },
+];
