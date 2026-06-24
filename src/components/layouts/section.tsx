03c7c2baaba2bc2c1701de91493bb04d95177230
Riza Yakub <afrizal.090477@gmail.com>
2026-06-24 08:54:12 +0700

    feat: add reusable section layout

---
+++ src/components/layouts/section.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+
+interface SectionProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Section({
+  children,
+  className,
+}: SectionProps) {
+  return (
+    <section
+      className={cn(
+        "relative w-full",
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+}
